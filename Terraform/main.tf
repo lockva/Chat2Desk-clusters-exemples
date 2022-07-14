79b26ebe1895b0a9a3f87aaae0d7a9a9e92d7404
Pablo <pablo0.myt@gmail.com>
2022-07-14 15:11:04 -0300

    Clustes ansible e terraform

---
+++ Terraform/main.tf
@@ -0,0 +1,31 @@
+provider "google" {
+  project = "chat2desk-devops-test"
+  region = "us-central1"
+  zone = "us-central1-c"
+}
+
+resource "google_container_cluster" "primary" {
+  name = var.cluster_name
+  location = "us-central1-c"
+  initial_node_count = 2
+}
+
+node_pool {
+  autoscaling {
+    max_node_count = 4
+    min_node_count = 2
+  }
+}
+
+node_config {
+
+  machine_type = var.machine_type
+
+  labels = {
+    node = "label-chat2desk"
+  }
+  
+tags = [
+  "dev-cluster-chat2desk"]
+  
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "cluster_name" {
+  type = string
+  default = "chat2desk-cluster"
+}
+
+variable "machine_type" {
+  type = string
+  default = "n1.standard-1"
+}

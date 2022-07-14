5a10ffb87d0d0ce1880d3c8441310303695d453a
Pablo <pablo0.myt@gmail.com>
2022-07-14 16:35:19 -0300

    v2
    Simplificando credenciais, ajustando chaves e endentação

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,6 +1,6 @@
 provider "google" {
   project = "chat2desk-cluster"
-  credentials = "${file("credentials.json")}"
+  credentials = file("C:\Users\Usuario\Documents\chat2desk-cluster-07abfe7947ae.json")
   region = "us-central1"
   zone = "us-central1-c"
 }
@@ -9,25 +9,30 @@ resource "google_container_cluster" "primary" {
   name = var.cluster_name
   location = "us-central1-c"
   initial_node_count = 2
-}
 
-node_pool {
-  autoscaling {
-    max_node_count = 4
-    min_node_count = 2
+  node_pool {
+    autoscaling {
+      max_node_count = 4
+      min_node_count = 2
+    }
   }
-}
 
-node_config {
+  node_config {
 
-  machine_type = var.machine_type
+    machine_type = var.machine_type
+
+    oauth_scopes = [
+      "http://www.google.apis.com/auth/loggin.write"
+      "http://www.google.apis.com/auth/monitoring"
+
+    ]
 
   labels = {
     node = "label-chat2desk"
   }
   
-tags = [
-  "dev-cluster-chat2desk"]
+  tags = [
+    "exemple-terraform-cluster"]
   
+  }
 }
-*/
